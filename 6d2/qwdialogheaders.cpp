18bebd0344582239c61d584f438c213c897f551a
baby7305 <songhaibei7305@163.com>
2020-11-19 15:36:25 +0800

    自定义对话框及其调用
    利用信号与槽实现交互操作

--- 6d2/qwdialogheaders.cpp
+++ 6d2/qwdialogheaders.cpp
@@ -1,8 +1,6 @@
 #include "qwdialogheaders.h"
 #include "ui_qwdialogheaders.h"
 
-#include    <QMessageBox>
-
 QWDialogHeaders::QWDialogHeaders(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::QWDialogHeaders)
@@ -14,7 +12,6 @@ QWDialogHeaders::QWDialogHeaders(QWidget *parent) :
 
 QWDialogHeaders::~QWDialogHeaders()
 {
-    QMessageBox::information(this,"提示","设置表头标题对话框被删除");
     delete ui;
 }
 
--- 6d2/qwdialogsize.h
+++ 6d2/qwdialogsize.h
@@ -18,8 +18,8 @@ public:
     explicit QWDialogSize(QWidget *parent = 0);
     ~QWDialogSize();
 
-    int     rowCount();//获取对话框输入的行数
-    int     columnCount();//获取对话框输入的列数
+    int rowCount();//获取对话框输入的行数
+    int columnCount();//获取对话框输入的列数
     void    setRowColumn(int row, int column); //初始对话框上两个SpinBox的值
 
 private slots:
